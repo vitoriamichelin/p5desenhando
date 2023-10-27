973842bac6b6febef65ea060544c0b92543b07a0
vitoriamichelin <148561823+vitoriamichelin@users.noreply.github.com>
2023-10-27 12:12:37 -0300

    adicionando arquivo css e js

---
+++ sktech.js
@@ -0,0 +1,15 @@
+
+function setup(){   
+
+    createCanvas(600,600); }
+  function draw() {                                          background("white"); 
+                   
+                fill("red"); 
+                   rect(0, 0, 100, 200);
+                   fill("white"); 
+                   rect(100, 0, 100, 200); 
+                   fill("green") ; 
+                   rect(200, 0, 100, 200); }
+  
+  
+  
